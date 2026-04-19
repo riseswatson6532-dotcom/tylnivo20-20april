4738ee10f0bd7ae73d0cdc66b3c5304ab4a929c0
riseswatson6532-dotcom <riseswatson6532@gmail.com>
2026-04-20 02:11:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,18 @@
 <html>
 
 <head>
+   <!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,5021635,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?5021635&101" alt="web stats" border="0"></a></noscript>
+<!-- Histats.com  END  -->
   <title>Mellow - Hotel HTML Website Template</title>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
